d1b0e382ebf782e16bb9055d9c20292712f413d7
youngplq10 <patryk.plq@o2.pl>
2025-03-25 21:43:19 +0100

    Added validation for creating new account

--- src/app/components/SignUpForm.tsx
+++ src/app/components/SignUpForm.tsx
@@ -1,27 +1,79 @@
+"use client"
+
 import { Button, Link, Typography } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
+import { validateNewUser } from '../scripts/validation';
 
 const SignUpForm = () => {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [repassword, setRepassword] = useState("");
+    const [thumbnail, setThumbnail] = useState<File | null>(null);
+
+    const handleSubmit = () => {
+        const validation = validateNewUser(username, email, password, repassword);
+    }
+
     return (
         <form>
-            <input type='text' className='form-control custom-input py-2 my-3' placeholder='Username' style={{
+            <input type='text' className='form-control custom-input py-2 my-3' placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} style={{
                 minHeight: "3rem",
                 backgroundColor: "#05040c",
                 color: "#e8e6f5",
                 border: "1px solid #e8e6f5",
             }} />
 
-            <input type='text' className='form-control custom-input py-2 my-3' placeholder='Password' style={{
+            <input type='text' className='form-control custom-input py-2 my-3' placeholder='E-mail' value={email} onChange={(e) => setEmail(e.target.value)} style={{
                 minHeight: "3rem",
                 backgroundColor: "#05040c",
                 color: "#e8e6f5",
                 border: "1px solid #e8e6f5",
             }} />
 
-            <Button variant='contained' className='px-4 mb-2'>Create account</Button>
-            <Typography variant='body1' color='text.primary'>You already have an account? <Link href="/sign-in">Sign in now!</Link></Typography>
+            <input type='password' className='form-control custom-input py-2 my-3' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} style={{
+                minHeight: "3rem",
+                backgroundColor: "#05040c",
+                color: "#e8e6f5",
+                border: "1px solid #e8e6f5",
+            }} />
+
+            <input type='password' className='form-control custom-input py-2 my-3' placeholder='Confirm Password' value={repassword} onChange={(e) => setRepassword(e.target.value)} style={{
+                minHeight: "3rem",
+                backgroundColor: "#05040c",
+                color: "#e8e6f5",
+                border: "1px solid #e8e6f5",
+            }} />
+
+            <input 
+                type="file" 
+                accept="image/*" 
+                id="file-upload"
+                className="d-none"
+                onChange={(e) => setThumbnail(e.target.files?.[0] || null)} 
+            />
+
+            <label 
+                htmlFor="file-upload" 
+                className="form-control py-3 my-3 text-center" 
+                style={{
+                    minHeight: "3rem",
+                    backgroundColor: "#05040c",
+                    color: "#e8e6f5",
+                    border: "1px solid #e8e6f5",
+                    cursor: "pointer",
+                }}
+            >
+                {thumbnail ? thumbnail.name : "Upload Profile Picture"}
+            </label>
+
+            <Button variant='contained' className='px-4 mb-2' onClick={handleSubmit}>Create account</Button>
+
+            <Typography variant='body1' color='text.primary'>
+                You already have an account? <Link href="/sign-in">Sign in now!</Link>
+            </Typography>
         </form>
     )
 }
 
-export default SignUpForm
+export default SignUpForm;
---
+++ src/app/scripts/validation.ts
@@ -0,0 +1,61 @@
+import Joi from "joi"
+
+const newUserSchema = Joi.object({
+    username: Joi.string().alphanum().min(3).max(20).required().messages({
+        "string.empty": "Username can't be empty.",
+        "string.min": "Username is too short. Minimum 3 characters.",
+        "string.max": "Username is too long. Maximum 20 characters.",
+        "any.required": "Username field is required to process.",
+        "string.alphanum": "Username doesn't only contain alphanumeric characters."
+    }),
+    email: Joi.string().email({ minDomainSegments: 2, tlds: false }).required().messages({
+        "string.empty": "Email can't be empty.",
+        "string.email": "Email is not valid."
+    }),
+    password: Joi.string().min(3).max(20).pattern(new RegExp('^[a-zA-Z0-9@#$%^&+=]{3,20}$')).required().messages({
+        "string.empty": "Password can't be empty.",
+        "string.pattern.base": "Password is not valid.",
+        "string.min": "Password is too short. Minimum 3 characters.",
+        "string.max": "Password is too long. Maximum 20 characters."
+    }),
+    repassword: Joi.any().valid(Joi.ref('password')).required().messages({
+        "any.only": "Passwords do not match.",
+        "any.required": "Please confirm your password."
+    }),
+})
+
+const existingUserSchema = Joi.object({
+    username: Joi.string().alphanum().min(3).max(20).required().messages({
+        "string.empty": "Username can't be empty.",
+        "string.min": "Username is too short. Minimum 3 characters.",
+        "string.max": "Username is too long. Maximum 20 characters.",
+        "any.required": "Username field is required to process.",
+        "string.alphanum": "Username doesn't only contain alphanumeric characters."
+    }),
+    password: Joi.string().min(3).max(20).required().messages({
+        "string.empty": "Password can't be empty.",
+        "string.pattern.base": "Password is not valid.",
+        "string.min": "Password is too short. Minimum 3 characters.",
+        "string.max": "Password is too long. Maximum 20 characters."
+    }),
+})
+
+export const validateNewUser = (getUsername: string, getEmail: string, getPassword: string, getRepassword: string) : string => {
+    const result = newUserSchema.validate({ username: getUsername, email: getEmail, password: getPassword, repassword: getRepassword});
+
+    if (result.error != null) {
+        return result.error.message;
+    }
+    
+    return "Success";
+}
+
+export const validateExistingUser = (getUsername: string, getPassword: string) : string => {
+    const result = existingUserSchema.validate({ username: getUsername, password: getPassword });
+
+    if (result.error != null) {
+        return result.error.message;
+    }
+    
+    return "Success";
+}
