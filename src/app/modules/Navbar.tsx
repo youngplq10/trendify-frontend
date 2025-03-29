3b98a84b4820ddb6dfae41dad84c4aae29cc126a
youngplq10 <patryk.plq@o2.pl>
2025-03-29 13:12:15 +0100

    Changed navbar to depend on user auth state

--- src/app/modules/Navbar.tsx
+++ src/app/modules/Navbar.tsx
@@ -1,8 +1,21 @@
+"use client"
+
 import { Typography } from '@mui/material'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getIsAuthenticated } from '../scripts/server'
 
 const Navbar = () => {
+    const [isLogged, setIsLogged] = useState(false);
+
+    useEffect(() => {
+        const fetchIsAuth = async () => {
+            const isAuth = await getIsAuthenticated();
+            setIsLogged(isAuth);
+        }
+        fetchIsAuth();
+    }, []);
+
     return (
         <nav className='my-3'>
             <div className="row">
@@ -10,9 +23,17 @@ const Navbar = () => {
                     <Link href="/" className='text-decoration-none'><Typography variant='h4' color='text.primary'>Trendify</Typography></Link>
                 </nav>
                 <nav className="col-auto ms-auto d-flex align-items-center gap-3">
-                    <Link href="/sign-in" className="text-decoration-none">
-                        <Typography variant="h4" color="text.primary">Sign in</Typography>
-                    </Link>
+                    {
+                        isLogged ? (
+                            <Link href="/log-out" className="text-decoration-none">
+                                <Typography variant="h4" color="text.primary">Logout</Typography>
+                            </Link>
+                        ) : (
+                            <Link href="/sign-in" className="text-decoration-none">
+                                <Typography variant="h4" color="text.primary">Sign in</Typography>
+                            </Link>
+                        )
+                    }
                 </nav>
             </div>
         </nav>
